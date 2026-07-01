b81a03af5cd20c5b6fcc28df2121277740a833ab
yusuf <yusuf210409@gmail.com>
2026-07-01 22:11:09 +0700

    adding route for server

--- lib/utilities/predict.dart
+++ lib/utilities/predict.dart
@@ -6,7 +6,7 @@ import 'package:frontend_burns/state/result_notifier.dart';
 import 'package:http/http.dart' as http;
 
 class Predict {
-  static const URL = "http://localhost:8000/predict";
+  static const URL = "http://3.26.104.4:8000/predict";
 
   static Future<void> predict(Uint8List image) async {
     final request = http.MultipartRequest("POST", Uri.parse(URL));
